a340b16477ac60c6d9eeb0fcc2ac5628ed4a833b
zirei <zeatgomez@gmail.com>
2021-09-27 19:59:32 -0500

    pasword

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 
 const config = {
-    dbUrl: process.env.DB_URL || 'mongodb+srv://zirei:Kobahi5*@cluster0.uf6mm.mongodb.net/cluster0?retryWrites=true&w=majority',
+    dbUrl: process.env.DB_URL || 'mongodb+srv://zirei:<pasword>*@cluster0.uf6mm.mongodb.net/cluster0?retryWrites=true&w=majority',
     port: process.env.PORT || 3001,
     host: process.env.HOST || 'http://localhost',
     publicRoute: process.env.PUBLIC_ROUTE || '/app',
